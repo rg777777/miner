a642dec5d7a7a689b2f3663cae6d408a6386e89c
Rafo <rg777777@gmail.com>
2017-06-27 17:36:48 +0400

    fix check only bombs

--- miner.js
+++ miner.js
@@ -37,59 +37,54 @@ const drawMines = (board, minCounts, i) => {
 const drawNumbers = (minBoard) => {
   for (let y = 0; y < minBoard.length; y++) {
     for (let x = 0; x < minBoard[0].length; x++) {
-      minBoard[x][y] = checkNeighbor(minBoard, x, y)
+      if (minBoard[x][y] === bomb) {
+        minBoard = changeNeighbors(minBoard, x, y)
+      }
     }
   }
   return minBoard
 }
 
-const checkNeighbor = (minBoard, x, y) => {
-  let count = 0
+const changeNeighbors = (minBoard, x, y) => {
   const rows = minBoard.length - 1
   const cols = minBoard[0].length - 1
-
-  if (minBoard[x][y] === bomb) {
-    return bomb
-  }
-
   const xNext = x + 1
   const xPrev = x - 1
   const yNext = y + 1
   const yPrev = y - 1
 
-  if (xNext <= rows && minBoard[xNext][y] === bomb) {
-    count += 1
+  if (xNext <= rows && minBoard[xNext][y] !== bomb) {
+    minBoard[xNext][y] += 1
   }
 
-  if (xPrev >= 0 && minBoard[xPrev][y] === bomb) {
-    count += 1
+  if (xPrev >= 0 && minBoard[xPrev][y] !== bomb) {
+    minBoard[xPrev][y] += 1
   }
 
-  if (yNext <= cols && minBoard[x][yNext] === bomb) {
-    count += 1
+  if (yNext <= cols && minBoard[x][yNext] !== bomb) {
+    minBoard[x][yNext] += 1
   }
 
-  if (yPrev >= 0 && minBoard[x][yPrev] === bomb) {
-    count += 1
+  if (yPrev >= 0 && minBoard[x][yPrev] !== bomb) {
+    minBoard[x][yPrev] += 1
   }
 
-  if (xNext <= rows && yNext <= cols && minBoard[xNext][yNext] === bomb) {
-    count += 1
+  if (xNext <= rows && yNext <= cols && minBoard[xNext][yNext] !== bomb) {
+    minBoard[xNext][yNext] += 1
   }
 
-  if (xPrev >= 0 && yNext <= cols && minBoard[xPrev][yNext] === bomb) {
-    count += 1
+  if (xPrev >= 0 && yNext <= cols && minBoard[xPrev][yNext] !== bomb) {
+    minBoard[xPrev][yNext] += 1
   }
 
-  if (xPrev >= 0 && yPrev >= 0 && minBoard[xPrev][yPrev] === bomb) {
-    count += 1
+  if (xPrev >= 0 && yPrev >= 0 && minBoard[xPrev][yPrev] !== bomb) {
+    minBoard[xPrev][yPrev] += 1
   }
 
-  if (xNext <= rows && yPrev >= 0 && minBoard[xNext][yPrev] === bomb) {
-    count += 1
+  if (xNext <= rows && yPrev >= 0 && minBoard[xNext][yPrev] !== bomb) {
+    minBoard[xNext][yPrev] += 1
   }
-
-  return count
+  return minBoard
 }
 
 const miner = (rows, cols, mineCount) => {
